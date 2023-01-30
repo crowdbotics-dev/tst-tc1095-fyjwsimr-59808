0429ed7da0bdf6799787b3177db72646fad2c12b
Crowdbotics <engineering@crowdbotics.com>
2023-01-30 00:41:02 +0000

    Update store.

---
+++ store/tSTTCfyjwsimrqa/api.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+const tSTTCfyjwsimrqa = axios.create({
+  baseURL: "https://www.dsconnect.com/api",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+export const apiService = {}
